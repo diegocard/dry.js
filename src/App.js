04d134c99f71e0522dc119d53569211c93448dc5
Diego Cardozo <diegocard@gmail.com>
2014-08-21 17:16:44 -0300

    Working on app routing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 air.App = function(name, options) {
     this.name = name;
     this.options = options || {};
-    this.routes = this.options.routes || {};
+    this.routes = this.options.routes || [];
     this.controllers = this.options.controllers || {};
     this.views = this.options.views || {};
 };
 
 air.App.prototype.controller = function(name, methods) {
+	var methodName, method;
     this.controllers[name] = new air.Controller(name, methods);
-    //TODO: Register routes for each controller method
+    // Register routes for each controller method
+    // TODO: Test, finish
+    if (methods && methods.length) {
+	    for (methodName in methods) {
+	    	if (methods.hasOwnProperty(methodName)) {
+    			this.routes.push(methodName);
+	    	}
+	    }
+    }
 };
 
 air.App.prototype.view = function(name, templateData) {
@@ -16,5 +25,5 @@ air.App.prototype.view = function(name, templateData) {
 };
 
 air.App.prototype.init = function() {
-    this.router = new air.Router(this.routes);
+    this.router = new air.Router(this.controllers, this.routes);
 };
--- src/Router.js
+++ src/Router.js
@@ -1,3 +1,24 @@
-air.Router = function(name) {
-    // TODO: Implement using vendor component
+air.Router = function(controllers, routes) {
+    // Implemented using vendor component (Routie)
+    // TODO: Test
+    var i, len, route, controllerName, controllerMethod;
+    for (i=0, len=routes.length; i<len; i++) {
+        route = routie[i];
+        controllerName = this.getControllerName(route);
+        controllerMethod = this.getControllerName(route);
+        if (controller) {
+            routie(route, function(params) {
+                // TODO: Convert params to object?
+                controllers[controllerName].invokeMethod(controllerMethod, params);
+            });
+        }
+    }
+};
+
+air.Router.prototype.getControllerName = function(route) {
+
+};
+
+air.Router.prototype.getControllerMethod = function(route) {
+
 };
